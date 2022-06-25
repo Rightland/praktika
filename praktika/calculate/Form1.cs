2d3c538e49b476c1aac2b290e82f10d1185d5b6d
Rightland <108046837+Rightland@users.noreply.github.com>
2022-06-25 18:51:34 +0600

    Add files via upload

---
+++ praktika/calculate/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double onen = Convert.ToDouble(textBox2.Text);
+            double twow = Convert.ToDouble(textBox3.Text);
+            var Plus = new addition();
+            textBox1.Text =(Plus.plus(onen,twow).ToString());
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double onen = Convert.ToDouble(textBox2.Text);
+            double twow = Convert.ToDouble(textBox3.Text);
+            textBox1.Text = (onen * twow).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double onen = Convert.ToDouble(textBox2.Text);
+            double twow = Convert.ToDouble(textBox3.Text);
+            if (twow != 0)
+            {
+                textBox1.Text = (onen / twow).ToString();
+            }
+            else
+            {
+                textBox1.Text = "second number can't be 0";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double onen = Convert.ToDouble(textBox2.Text);
+            double twow = Convert.ToDouble(textBox3.Text);
+            textBox1.Text = (onen - twow).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double onen = Convert.ToDouble(textBox2.Text);
+            double twow = Convert.ToDouble(textBox3.Text);
+            if (twow != 0)
+            {
+                textBox1.Text = (onen % twow).ToString();
+            }
+            else
+            {
+                textBox1.Text = "second number can't be 0";
+            }
+        }
+    }
+}
+
+
